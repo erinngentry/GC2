7d2e8074937f925b68abeff678cdf196b9e8ec3c
Cassindra Washington <31632801+CassindraW@users.noreply.github.com>
2019-12-03 15:08:45 -0500

    Update game_script.js

--- templates/game_script.js
+++ templates/game_script.js
@@ -2,51 +2,50 @@ var createdpattern = [];
 var userpattern = [];
 var level = 0;
 
-$("gameimport.html").ready(function () {
-    grid(3, 3);
+$("index.html").ready(function() {
+  grid(3,3);
 });
 
-// $(".button").click(function(e) {
-//   var size = prompt("Please select your grid size");
-//   grid(size, size);
-// });
-
-$("td").click(function (e) {
-    var userClicked = $(this).attr("id");
-    console.log("button clicked");
-})
 
 //button that determines grid size AxA
 function boxclick() {
-    console.log("grid size button clicked");
-    var size = prompt("Please select your grid size");
-    grid(size, size);
+	console.log("grid size button clicked");
+	var size = prompt("Please select your grid size");
+  grid(size, size);
 }
+
 
 //makes the grid
 function grid(rows, cols) {
-    var table = "<table>";
-    var size = (1 / rows * 525) + "px";
+  var table = "<table>";
+  var size = (1 / rows * 525) + "px";
+	var blockOrder = []; 
+		
 
-
-    for (i = 0; i < rows; i++) {
-        table += "<tr>";
-        for (j = 0; j < cols; j++) {
-            table += "<td>" + "</td>";
-        }
-        table += "</tr>";
+  for (i=0; i<rows; i++) {
+    table += "<tr>";
+    for (j=0; j<cols; j++) {
+      table += "<td>"+"</td>";
+			blockOrder.push([i,j]);
+			console.log(blockOrder.join('\n'));    //puts into an array
     }
-    table += "</table>";
-
-    $("#container").empty().append(table);
-    $("tr").css("height", size);
-    $("td").css("color", "red").css("width", size);
-    table.on("click", "td", makepattern());
-
+    table += "</tr>";
+  }
+  table += "</table>";
+  
+  $("#container").empty().append(table);
+  $("tr").css("height", size);
+  $("td").css("color", "red").css("width", size);
+	return blockOrder;
 }
 
 
-function makepattern() {
-    console.log("make pattern button clicked");
+function makepattern(){
+	console.log("make pattern button clicked");
 
+
+	$('body').on('click', 'td',function(e){
+	var userClicked= $(this).attr("id");
+	console.log("button clicked");
+	})
 }
